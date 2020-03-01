b6cb6195821e473b45bab9c9007a870eb13b7596
spockwang <wbbtiger@gmail.com>
2020-03-01 14:23:18 +0800

    test script

--- src/test.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-rm -f /tmp/test.log
-for ((i=0; i < 200; i++)); do
-    echo "round $i" >> /tmp/test.log
-    (go test -run TestChallenge1Concurrent) >> /tmp/test.log
-    grep -nr "FAIL.*" /tmp/test.log
-done
